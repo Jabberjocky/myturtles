7d872a1af2f0deadd85868fc844e8ef92c995330
Jabberjocky <sebastian.boo.pj@gmail.com>
2017-02-22 14:41:52 +0200

    added dartboard

---
+++ dartboard.py
@@ -0,0 +1,24 @@
+import turtle
+import math
+wn = turtle.Screen()
+boo = turtle.Turtle()
+boo.speed(99999)
+
+angle = 4
+distance = 1
+why = 0
+
+
+def circle():
+    for i in range(revos):
+        boo.fd(distance)
+        boo.rt(angle)
+
+for i in range(5):
+    angle = angle / 2
+    why += 50   
+
+
+
+    
+
